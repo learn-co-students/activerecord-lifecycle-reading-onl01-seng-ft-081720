c42ac18e207814ef84d7f09b4f1e8a616cdda549
Newland-A <ajn252@gmail.com>
2020-10-27 13:15:57 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  #new code!!
+  #is called after validations so it never makes it to this
+  # before_save :make_title_case
+
+  # change to before_validation, this happens before validation
+  before_validation :make_title_case
+
+  #new code
+  #use before_save when validations wont interfere with the methods
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -12,6 +23,11 @@ class Post < ActiveRecord::Base
   end
 
   def make_title_case
+    #Rails provides a String#titlecase method
     self.title = self.title.titlecase
+  end
+
+  def email_author_about_post
+
   end
 end
